4b64478f3072eec79be34b694f8a193012bf5956
luttt <51191255+luttt@users.noreply.github.com>
2020-11-21 03:49:32 +0900

    push alarm is implemented.

--- Client/app/src/main/java/com/example/client/Server.java
+++ Client/app/src/main/java/com/example/client/Server.java
@@ -15,7 +15,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class Server {
+public class Server extends AppCompatActivity{
 
     public void activePost() {
         try{
